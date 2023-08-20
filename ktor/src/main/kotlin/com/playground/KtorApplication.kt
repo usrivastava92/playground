6dd912836a925526dacabbad726a458855f60edf
utkarsh <usrivastava92@gmail.com>
2023-08-20 18:23:36 +0530

    fix ktor routing

--- ktor/src/main/kotlin/com/playground/KtorApplication.kt
+++ ktor/src/main/kotlin/com/playground/KtorApplication.kt
@@ -1,12 +1,14 @@
 package com.playground
 
-import io.ktor.server.application.*
-import com.playground.plugins.*
+import com.playground.plugins.configureRouting
+import io.ktor.server.application.Application
+import io.ktor.server.netty.EngineMain
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
-@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+// application.conf references the main function.
+// This annotation prevents the IDE from marking it as unused.
+@Suppress("unused")
 fun Application.module() {
     configureRouting()
 }
