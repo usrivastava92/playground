b303f0940b5cf65a525c1a8bceec0107c4088434
Utkarsh Srivastava <usrivastava92@gmail.com>
2023-08-21 13:40:16 +0530

    Update build-python.sh

--- bin/build-python.sh
+++ bin/build-python.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd python
+
+if [ -z "$(find . -name '*.py')" ]; then
+  echo "No Python files found. Skipping build and test."
+  exit 0
+fi
+
 python -m pip install --upgrade pip
 pip install ruff pytest
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
@@ -8,10 +15,5 @@ if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 ruff --format=github --select=E9,F63,F7,F82 --target-version=py37 .
 # default set of ruff rules with GitHub Annotations
 ruff --format=github --target-version=py37 .
-
-if [ -z "$(find . -name '*.py')" ]; then
-  echo "No Python files found. Skipping build and test."
-  exit 0
-fi
 
 pytest
